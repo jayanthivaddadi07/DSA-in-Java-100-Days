4678f74872be6b5e8770deeb8a973a9574d99afc
jayanthivaddadi07 <jayanthivaddadi07@gmail.com>
2026-06-17 14:06:28 +0530

    day 1 file added

---
+++ Day1.java
@@ -0,0 +1,14 @@
+public class Day1 {
+    public static void main(String[] args) {
+
+        //sum of the n number
+
+        int sum=0;
+        for(int i=0;i<10;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+        
+    }
+    
+}
