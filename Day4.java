08bae850aada849065ea19823b8942b03a9c25b2
jayanthivaddadi07 <jayanthivaddadi07@gmail.com>
2026-06-18 06:57:37 +0530

    Day4 file added

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class Day4 {
+    public static void main(String[] args) {
+        int x=5;
+        switch(x){
+            case 1:
+                System.out.println("A");
+            default:
+                System.out.println("D");
+            case 2 :
+                System.out.println("B");
+
+        }
+    }
+    
+}
