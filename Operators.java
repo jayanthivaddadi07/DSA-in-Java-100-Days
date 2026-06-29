338a42f96945167a6799260bef8edb873fb7788a
jayanthivaddadi07 <jayanthivaddadi07@gmail.com>
2026-06-29 23:15:58 +0530

    operators file added

---
+++ Operators.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        // int a=21;
+        // int b=40;
+        // int c=a+b;
+        // System.out.println(c);
+
+        // Scanner sc=new Scanner(System.in);
+        //     int a=sc.nextInt();
+            // int b=sc.nextInt();
+        //     int sum=a+b;
+             // System.out.println("sum is:" +sum);
+
+             //ASSIGNMENT
+        // int a=sc.nextInt();
+        // System.out.println(a%=3);
+            
+        //RELATIONAL
+        // if(a==b){
+        //     System.out.println("true");
+        // }
+        // else{
+        //     System.out.println("false");
+        // }
+
+        //LOGICAL
+    //     if(a>=5 &&a<=10){
+    //     System.out.println(1);
+        
+    // }
+    // else{
+    //     System.out.println(0);
+    // }
+ 
+    //TERNARY OPERATOR
+    // int max=(a%2!=0)?0:1;
+    // System.out.println(max);
+
+//     int a = 11, b = 5, c = 2;
+// int result = a / b * c;
+// System.out.println(result);
+ 
+//TYPE CONVERSION---LOW DATATYPE--HIGH DATATYPE
+// float result=(float)a/b;
+// System.out.println(result);
+
+//TYPE CASTING---HIGH --LOW DATATYPE
+double x = 9.99;
+int y = (int) x;  // Manually cast double to int
+System.out.println(y);
+
+    
+}
+}
