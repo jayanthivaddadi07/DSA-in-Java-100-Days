51f7b0be0f58485b7b6d4f040ef62d317f021b5c
jayanthivaddadi07 <jayanthivaddadi07@gmail.com>
2026-06-30 09:26:30 +0530

    Arraymethods added

---
+++ ArrayMethods.java
@@ -0,0 +1,39 @@
+ import java.util.Arrays;
+public class ArrayMethods {
+    public static void main(String[] args) {
+        // int arr[];
+        // arr=new int[10];
+        // 
+        // int[] brr={10,2,3,5,70,21,8};
+        // Arrays.sort(brr); 
+        //  Arrays.toString(brr);    
+        // for(int x:brr){
+        //     System.out.println(x);
+        // }
+        // System.out.println(brr);
+        // System.out.println(Arrays.toString(brr));
+        // int[] arr=new int[10];
+        // Arrays.fill(arr,100);
+        // System.out.println(Arrays.toString(arr));
+      // int a[]={1,2,3};
+      // int b[]={1,2};
+      // System.out.println(Arrays.equals(a,b));
+
+      int[] arr={5,2,9,1,7};
+      // System.err.println(arr.length);
+      // Arrays.sort(arr);
+      // System.out.println(Arrays.toString(arr));
+      // // Arrays.fill(arr,10);
+      // // System.out.println(Arrays.toString(arr));
+      // int idx=Arrays.binarySearch(arr,7);
+      // System.out.println(idx);
+      // int[] newArr=Arrays.copyOf(arr,7);
+      // System.out.println(Arrays.toString(newArr));
+      int[] arr1={9,2,9,1,7};
+      System.out.println(Arrays.equals(arr,arr1));
+
+    }
+
+    
+}
+ 
